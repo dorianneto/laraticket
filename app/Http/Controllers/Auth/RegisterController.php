4e0ee996ad16ac93d09831dd65648a2bdf5a8ac5
Dorian Neto <doriansampaioneto@gmail.com>
2017-10-23 14:41:54 -0300

    Apply role to user on register

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -62,10 +62,14 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-        return User::create([
+        $user = User::create([
             'name' => $data['name'],
             'email' => $data['email'],
-            'password' => bcrypt($data['password']),
+            'password' => bcrypt($data['password'])
         ]);
+
+        $user->roles()->attach(2);
+
+        return $user;
     }
 }
--- app/User.php
+++ app/User.php
@@ -26,4 +26,14 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    /**
+     * Undocumented function
+     *
+     * @return void
+     */
+    public function roles()
+    {
+        return $this->belongsToMany(\App\Role::class);
+    }
 }
