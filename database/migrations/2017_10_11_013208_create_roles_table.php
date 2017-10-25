533b672eb464bea2843d1d26586032599187a791
Dorian Neto <doriansampaioneto@gmail.com>
2017-10-25 19:05:26 -0300

    Fix migration to run on mysql 5.6 version

--- database/migrations/2017_10_11_013208_create_roles_table.php
+++ database/migrations/2017_10_11_013208_create_roles_table.php
@@ -17,7 +17,7 @@ class CreateRolesTable extends Migration
             $table->increments('id');
             $table->string('name');
             $table->string('slug')->unique();
-            $table->jsonb('permissions');
+            $table->text('permissions');
             $table->timestamps();
         });
     }
